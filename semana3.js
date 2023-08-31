41e4a06ff4c333c9ab1c0731983dc6dca32b6e50
geovanagv <geovana.veloso@iesb.edu.br>
2023-08-30 21:36:03 -0300

    semana4

---
+++ semana3.js
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+export default function semana3() {
+  return (
+    <View>
+      <Text>semana3</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
