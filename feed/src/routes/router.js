14b1a0e96e6a25d6688e218c66afbb2c8da2c485
geovanagv <geovana.veloso@iesb.edu.br>
2023-09-26 21:16:08 -0300

    feed

---
+++ feed/src/routes/router.js
@@ -0,0 +1,19 @@
+import { NavigationContainer } from '@react-navigation/native'
+import React from 'react'
+
+import { createStackNavigater } fron '@react-navigation/stack'
+
+const Stack = createStackNavigator()
+
+export default function router() {
+  return (
+
+
+   
+
+
+
+  )
+}
+
+const styles = StyleSheet.create({})
